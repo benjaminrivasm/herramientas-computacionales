b29f7e827dafd7ec05574b89119b53781880a4f1
Rivas <benjamin.rivas.m@ug.uchile.cl>
2025-08-11 16:27:42 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let d;
+let colorDot = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(120, 68, 187);
 }
 
 function draw() {
-  background(120, 68, 187);
+  d = random(10, 60);
+
+  colorMode(HSB);
+
+  fill(colorDot, 100, 100);
+  noStroke();
+  ellipse(mouseX, mouseY, d, d);
+
+  colorDot += 1;
+  if (colorDot > 360) {
+    colorDot = 0;
+  }
 }
