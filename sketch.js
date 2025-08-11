304a8cdf180f7e7bcb4c3c68df186fd8560ceb91
Rivas <benjamin.rivas.m@ug.uchile.cl>
2025-08-11 16:34:38 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let d;
+let colorDot = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  background(120, 68, 187);
+}
+
+function draw() {
+  d = random(10, 60);
+  
+  colorMode(HSB);
+  
+  fill(colorDot, 100, 100);
+  ellipse(mouseX, mouseY, d, d);
+  
+      colorDot += 1;
+  if (colorDot > 360) {
+    colorDot = 0;
+  }
+}
