67cc8b40ea093160ff3148ef59fdb46be09be26b
Benjamín Rivas <benjamin.rivas.m@ug.uchile.cl>
2025-08-25 18:10:43 -0400

    Add files via upload

---
+++ clase-01-02/sketch.js
@@ -0,0 +1,28 @@
+let d;
+let colorDot = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight).position(0,0).style('z-index','-1');
+  background(120, 68, 187);
+}
+
+function draw() {
+  d = random(10, 60);
+  
+  colorMode(HSB);
+  
+  fill(colorDot, 100, 100);
+  noStroke();
+  ellipse(mouseX, mouseY, d, d);
+  
+  colorDot += 1;
+  if (colorDot > 360) {
+    colorDot = 0;
+  }
+}
+
+function keyPressed() {
+  if (key === 'p') {
+    save('dibujo.jpg');
+  }
+}
